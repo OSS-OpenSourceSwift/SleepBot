813e9902aa6f147bedc9ca322c32d243f7bb50e4
jjgarzella <jjgarzella@gmail.com>
2015-01-01 12:48:36 -0700

    Added an NSDateHacks file to help show the stats of each separate nap. Also, made the start time persist app launches.

---
+++ SleepBot/SleepBot/NSDateHacks.swift
@@ -0,0 +1,69 @@
+//
+//  NSDateHacks.swift
+//  SleepBot
+//
+//  Created by JJ Garzella on 1/1/15.
+//  Copyright (c) 2015 JMGSE. All rights reserved.
+//
+
+import Foundation
+
+public enum DateClipInterval {
+    case ThirtyMinutes
+    case OneHour
+    
+    typealias DateClippingBlock = (NSDateComponents) -> NSDateComponents
+    
+    private func clipBlock() -> DateClippingBlock {
+        switch self {
+        case .ThirtyMinutes:
+            return { (components: NSDateComponents) -> NSDateComponents in
+                if (components.minute < 30) {
+                    components.minute = 0
+                } else if (components.minute >= 30) {
+                    components.minute = 0
+                    components.hour += 1
+                }
+                return components
+            }
+        case .OneHour:
+            return { (components: NSDateComponents) -> NSDateComponents in
+                if (components.minute < 15) {
+                    components.minute = 0
+                } else if ((15 <= components.minute && components.minute < 45)) {
+                    components.minute = 30
+                } else if (45 <= components.minute) {
+                    components.minute = 0
+                    components.hour += 1
+                }
+                return components;
+            }
+        }
+    }
+}
+
+extension NSDate {
+    // MARK - clipping
+    
+    
+    public class func closestIntervalToDate(date: NSDate, interval: DateClipInterval) -> NSDate? {
+        let cal = NSCalendar.currentCalendar()
+        let flags: NSCalendarUnit = .CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay | .CalendarUnitHour | .CalendarUnitMinute
+        let componentsOfDate = cal.components(flags, fromDate: date)
+        let newComponents = interval.clipBlock()(componentsOfDate)
+        return cal.dateFromComponents(newComponents)
+    }
+    
+    // MARK - description between two times
+    
+    public func descriptionBetweenDate(date: NSDate) -> String {
+        let cal = NSCalendar.currentCalendar()
+        let flags: NSCalendarUnit = .CalendarUnitDay | .CalendarUnitHour | .CalendarUnitMinute
+        let components = cal.components(flags, fromDate: self, toDate: date, options: NSCalendarOptions.allZeros)
+        var descBetween: String = ""
+        if (components.day > 0) {descBetween += "Days: \(components.day)\n"}
+        if (components.hour > 0) {descBetween += "Hours: \(components.hour)\n"}
+        descBetween += "Minutes: \(components.minute)"
+        return descBetween
+    }
+}
--- SleepBot/SleepBot/SleepEventViewController.swift
+++ SleepBot/SleepBot/SleepEventViewController.swift
@@ -15,7 +15,8 @@ enum SleepEventMode {
     case OutOfBed
     case Recorded
     
-    func labelStrings() -> (String, String){
+    func labelStrings() -> (String, String)
+    {
         switch self {
         case .NotStarted:
             return ("Tap to start sleeping", "Go to bed")
@@ -42,8 +43,31 @@ class SleepEventViewController: UIViewController {
         }
     }
     
-    var startTime: NSDate?
-    var endTime: NSDate?
+    var inProgress: Bool {
+        get {
+            return NSUserDefaults.standardUserDefaults().boolForKey("IN_PROGRESS")
+        }
+        set {
+            NSUserDefaults.standardUserDefaults().setBool(newValue, forKey: "IN_PROGRESS")
+        }
+    }
+    
+    var startTime: NSDate? {
+        get {
+            return NSUserDefaults.standardUserDefaults().objectForKey("START_TIME") as? NSDate
+        }
+        set {
+            NSUserDefaults.standardUserDefaults().setObject(newValue, forKey: "START_TIME")
+        }
+    }
+    var endTime: NSDate? {
+        get {
+            return NSUserDefaults.standardUserDefaults().objectForKey("END_TIME") as? NSDate
+        }
+        set {
+            NSUserDefaults.standardUserDefaults().setObject(newValue, forKey: "END_TIME")
+        }
+    }
     
     var duration: NSTimeInterval? {
         if let st = startTime {
@@ -54,15 +78,21 @@ class SleepEventViewController: UIViewController {
     
     @IBOutlet weak var infoLabel: UILabel!
     @IBOutlet weak var infoButton: UIButton!
-    
-    
+    @IBOutlet weak var statTextView: UITextView!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        mode = .NotStarted
+        if inProgress {
+            // In this case, the start time has already been entered
+            mode = .InBed
+        } else {
+            mode = .NotStarted
+        }
     }
 
-    func recordSleepTimes() {
+    func recordSleepTimes()
+    {
         let sleepType = HKCategoryType.categoryTypeForIdentifier(HKCategoryTypeIdentifierSleepAnalysis)
         let inBedValue = HKCategoryValueSleepAnalysis.InBed.rawValue
         let sleepSample = HKCategorySample(type: sleepType, value: inBedValue, startDate: startTime, endDate: endTime)
@@ -72,26 +102,38 @@ class SleepEventViewController: UIViewController {
             }
         }
     }
-    
 
-    @IBAction func update(sender: AnyObject) {
+    @IBAction func update(sender: AnyObject)
+    {
         println("Update sent by: \(sender)")
         switch mode {
         case .NotStarted:
             startTime = NSDate(timeIntervalSinceNow: 0)
             mode = .InBed
+            inProgress = true
         case .InBed:
             endTime = NSDate(timeIntervalSinceNow: 0)
             mode = .OutOfBed
-//            infoLabel.text = infoLabel.text! + "\nLasted: \(duration)"
+            inProgress = false
+            statTextView.text = startTime?.descriptionBetweenDate(endTime!)
+            println(startTime)
+            println(endTime)
         case .OutOfBed:
             recordSleepTimes()
             mode = .Recorded
+            startTime = nil
+            endTime = nil
         default:
             break
         }
     }
-
     
+    deinit {
+        if (!inProgress) {
+            startTime = nil
+            endTime = nil
+        }
+    }
+
 
 }
